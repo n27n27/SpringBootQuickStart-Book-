2eed01777825b6349c9ef785e4d85f182814171b
DESKTOP-JQ56L0P\skfdk <skfdkfkalsrn@gmail.com>
2021-03-21 08:47:47 +0900

    스프링부트퀵스타트책공부

---
+++ Chapter01/src/main/java/com/spring/OtherController.java
@@ -0,0 +1,12 @@
+package com.spring;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class OtherController
+{
+	public OtherController()
+	{
+		System.out.println("===> OtherContorller 생성");
+	}
+}
