9589d06774a218ed24403f98a723fd02925c62b4
DESKTOP-JQ56L0P\skfdk <skfdkfkalsrn@gmail.com>
2021-03-23 20:06:24 +0900

    시큐리티 마무리

--- Chapter07/src/main/java/com/springboot/config/SecurityConfig.java
+++ Chapter07/src/main/java/com/springboot/config/SecurityConfig.java
@@ -1,14 +1,19 @@
 package com.springboot.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
+	@Autowired
+	private BoardUserDetailsService boardUserDetailsService;
+	
 	@Override
 	protected void configure(HttpSecurity security) throws Exception
 	{
@@ -21,19 +26,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 		security.formLogin().loginPage("/login").defaultSuccessUrl("/loginSuccess", true);
 		security.exceptionHandling().accessDeniedPage("/accessDenied");
 		security.logout().invalidateHttpSession(true).logoutSuccessUrl("/login");
+		
+		security.userDetailsService(boardUserDetailsService);
 	}
 	
-	@Autowired
-	public void authenticate(AuthenticationManagerBuilder auth) throws Exception
+	@Bean
+	public PasswordEncoder passwordEncoder()
 	{
-		auth.inMemoryAuthentication().
-		withUser("manager").
-		password("{noop}manager123").
-		roles("MANAGER");
-		
-		auth.inMemoryAuthentication().
-		withUser("admin").
-		password("{noop}admin123").
-		roles("ADMIN");
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
+	
 }
---
+++ Chapter07/src/test/java/com/springboot/PasswordEncoderTest.java
@@ -0,0 +1,37 @@
+package com.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.springboot.domain.Member;
+import com.springboot.domain.Role;
+import com.springboot.persistence.MemberRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Commit
+public class PasswordEncoderTest
+{
+	@Autowired
+	private MemberRepository memberRepo;
+	
+	@Autowired
+	private PasswordEncoder encoder;
+	
+	@Test
+	public void testInsert()
+	{
+		Member member = new Member();
+		member.setId("manager2");
+		member.setPassword(encoder.encode("manager456"));
+		member.setName("매니저2");
+		member.setRole(Role.ROLE_MANAGER);
+		member.setEnabled(true);
+		memberRepo.save(member);
+	}
+}
